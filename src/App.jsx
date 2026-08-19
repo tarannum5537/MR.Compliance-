adaa9f334030055ff992c1321a2bc849cf35d075
rida <salman65.co@gmail.com>
2026-08-20 02:38:45 +0530

    Fix build errors and contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Toaster } from "react-hot-toast";
 import ScrollToTop from "./components/common/ScrollToTop";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   return (
     <>
+     <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+        }}
+      />
       <ScrollToTop />
       <AppRoutes />
     </>
