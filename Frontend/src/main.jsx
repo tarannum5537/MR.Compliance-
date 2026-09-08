d90e3032203a3d733879c4d90d0896ba3d84fd35
rida <salman65.co@gmail.com>
2026-09-08 15:29:19 +0530

    Fix Helmet provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<BrowserRouter future={{ v7_startTransition: true }}>
-  <App />
-</BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter future={{ v7_startTransition: true }}>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
